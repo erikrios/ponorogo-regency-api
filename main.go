38f689b7b56a11fc3370df81bdff40f79d92b1f0
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-06 13:28:31 +0700

    Create the database instance

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/erikrios/ponorogo-regency-api/config"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading .env file: %s\n", err.Error())
+	}
+
+	db, err := config.NewPostgreSQLDatabase()
+	if err != nil {
+		log.Fatalln(err.Error())
+	} else {
+		log.Printf("Successfully connected to database with instance address: %p", db)
+	}
 }
