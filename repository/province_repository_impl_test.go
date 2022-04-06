b4a174d1f995788ad16266f826b6151f623352e3
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-06 19:42:49 +0700

    Create the unit test for FindAll() method

--- repository/province_repository_impl_test.go
+++ repository/province_repository_impl_test.go
@@ -5,27 +5,65 @@ import (
 	"log"
 	"testing"
 
-	"github.com/erikrios/ponorogo-regency-api/config"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/erikrios/ponorogo-regency-api/entity"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
-	if err := godotenv.Load("./../.env"); err != nil {
+	if err := godotenv.Load("./../.env.local"); err != nil {
 		log.Fatalf("Error loading .env file: %s\n", err.Error())
 	}
 }
 
 func TestFindAll(t *testing.T) {
-	db, err := config.NewPostgreSQLDatabase()
+	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatal(err)
 	}
-	var repo ProvinceRepository = NewProvinceRepositoryImpl(db)
+	defer db.Close()
 
-	provinces, err := repo.FindAll(context.Background())
-	if err != nil {
-		t.Fatal(err)
+	expectedProvinces := []entity.Province{
+		{
+			ID:   "32",
+			Name: "Jawa Timur",
+		},
 	}
 
-	t.Log(provinces)
+	returnedRows := sqlmock.NewRows([]string{"id", "name"})
+	for _, province := range expectedProvinces {
+		returnedRows.AddRow(province.ID, province.Name)
+	}
+
+	t.Run("it should return valid provinces, when database successfully return the data", func(t *testing.T) {
+		mock.ExpectQuery("SELECT p.id, p.name FROM provinces p;").WillReturnRows(returnedRows)
+
+		var repo ProvinceRepository = NewProvinceRepositoryImpl(db)
+
+		got, err := repo.FindAll(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Fatal(err)
+		}
+
+		assert.ElementsMatch(t, expectedProvinces, got)
+	})
+
+	t.Run("it should return error, when database return an error", func(t *testing.T) {
+		mock.ExpectQuery("SELECT p.id, p.name FROM provinces p;").WillReturnError(ErrDatabase)
+
+		var repo ProvinceRepository = NewProvinceRepositoryImpl(db)
+
+		if _, err := repo.FindAll(context.Background()); assert.Error(t, err) {
+			assert.Equal(t, ErrDatabase, err)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Fatal(err)
+		}
+	})
 }
