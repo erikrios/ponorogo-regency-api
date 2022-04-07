1840ba804e0c37bcabbef5e4766fedb13a2cc85a
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-08 00:23:25 +0700

    Add swagger documentation

--- controller/home_controller.go
+++ controller/home_controller.go
@@ -17,7 +17,22 @@ func (h *HomeController) Route(e *echo.Echo) {
 	e.GET("/", h.GetHello)
 }
 
+// GetHello godoc
+// @Summary      Check the API Connectivity
+// @Description  Show the hello message
+// @Tags         home
+// @Accept       json
+// @Produce      json
+// @Success      200  {object} response
+// @Router       / [get]
 func (h *HomeController) GetHello(c echo.Context) error {
 	data := "Hello, World"
 	return c.JSON(http.StatusOK, model.NewResponse("success", "successfully get message", &data))
+}
+
+// This struct used for swaggo to generate the API documentation, as it doesn't support generic yet.
+type response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Data    string `json:"data"`
 }
