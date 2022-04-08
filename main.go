71ab6d0700147cefa4ccda99339b68f278eae99d
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-08 11:26:33 +0700

    Implements RemoveTrailingSlash middleware and provinces controller

--- main.go
+++ main.go
@@ -9,25 +9,27 @@ import (
 	"github.com/erikrios/ponorogo-regency-api/controller"
 	_ "github.com/erikrios/ponorogo-regency-api/docs"
 	"github.com/erikrios/ponorogo-regency-api/middleware"
+	"github.com/erikrios/ponorogo-regency-api/repository"
+	"github.com/erikrios/ponorogo-regency-api/service"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-// @title Ponorogo Regency API
-// @version 1.0
-// @description API for Administrative Subdivisions of Ponorogo Regency (Districts and Villages).
-// @termsOfService http://swagger.io/terms/
+// @title           Ponorogo Regency API
+// @version         1.0
+// @description     API for Administrative Subdivisions of Ponorogo Regency (Districts and Villages).
+// @termsOfService  http://swagger.io/terms/
 
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
 
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost
-// @BasePath /v1
+// @host      localhost
+// @BasePath  /v1
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if err := godotenv.Load(".env"); err != nil {
@@ -43,7 +45,12 @@ func main() {
 
 	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 
+	provinceRepository := repository.NewProvinceRepositoryImpl(db)
+
+	provinceService := service.NewProvinceServiceImpl(provinceRepository)
+
 	homeController := controller.NewHomeController()
+	provincesController := controller.NewProvincesController(provinceService)
 
 	e := echo.New()
 
@@ -53,12 +60,17 @@ func main() {
 		middleware.RateLimiter(e)
 		middleware.Recover(e)
 		middleware.Secure(e)
+		middleware.RemoveTrailingSlash(e)
 	} else {
 		middleware.Logger(e)
+		middleware.RemoveTrailingSlash(e)
 	}
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	homeController.Route(e)
+
+	g := e.Group("/api/v1")
+	provincesController.Route(g)
 
 	e.Logger.Fatal(e.Start(port))
 }
