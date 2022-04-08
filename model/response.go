da9090bcf76d5ce84e617c280f3420648ef4445d
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-08 11:26:00 +0700

    Change the generic type

--- model/response.go
+++ model/response.go
@@ -3,10 +3,10 @@ package model
 type Response[T any] struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
-	Data    *T     `json:"data"`
+	Data    T      `json:"data"`
 }
 
-func NewResponse[T any](status string, message string, data *T) *Response[T] {
+func NewResponse[T any](status string, message string, data T) *Response[T] {
 	return &Response[T]{
 		Status:  status,
 		Message: message,
