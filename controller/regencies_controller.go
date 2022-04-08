be6690cbf65f2dfb1b67e19de2f8ff933f85950f
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-08 13:15:28 +0700

    Refactor the code

---
+++ controller/regencies_controller.go
@@ -0,0 +1,84 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/erikrios/ponorogo-regency-api/model"
+	"github.com/erikrios/ponorogo-regency-api/service"
+	"github.com/labstack/echo/v4"
+)
+
+type regenciesController struct {
+	service service.RegencyService
+}
+
+func NewRegenciesController(service service.RegencyService) *regenciesController {
+	return &regenciesController{service: service}
+}
+
+func (r *regenciesController) Route(g *echo.Group) {
+	group := g.Group("/regencies")
+	group.GET("", r.getAll)
+	group.GET("/:id", r.getByID)
+}
+
+// GetAll	     godoc
+// @Summary      Get Regencies
+// @Description  Get regencies
+// @Tags         regencies
+// @Accept       json
+// @Produce      json
+// @Param        keyword  query     string  false  "regency name search by keyword"
+// @Success      200      {object}  regenciesResponse
+// @Failure      500      {object}  echo.HTTPError
+// @Router       /api/v1/regencies [get]
+func (r *regenciesController) getAll(c echo.Context) error {
+	keyword := c.QueryParam("keyword")
+
+	regencies, err := r.service.GetAll(context.Background(), keyword)
+	if err != nil {
+		return newErrorResponse(err)
+	}
+
+	response := model.NewResponse("success", "successfully get regencies", regencies)
+	return c.JSON(http.StatusOK, response)
+}
+
+// GetByID       godoc
+// @Summary      Get Regency by ID
+// @Description  get regencies by ID
+// @Tags         regencies
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "Regency ID"
+// @Success      200  {object}  regencyResponse
+// @Failure      404  {object}  echo.HTTPError
+// @Failure      500  {object}  echo.HTTPError
+// @Router       /api/v1/regencies/{id} [get]
+func (r *regenciesController) getByID(c echo.Context) error {
+	id := c.Param("id")
+
+	regency, err := r.service.GetByID(context.Background(), id)
+	if err != nil {
+		return newErrorResponse(err)
+	}
+
+	response := model.NewResponse("success", fmt.Sprintf("successfully get province with ID %s", id), regency)
+	return c.JSON(http.StatusOK, response)
+}
+
+// regenciesResponse struct is used for swaggo to generate the API documentation, as it doesn't support generic yet.
+type regenciesResponse struct {
+	Status  string          `json:"status"`
+	Message string          `json:"message"`
+	Data    []model.Regency `json:"data"`
+}
+
+// regencyResponse struct is used for swaggo to generate the API documentation, as it doesn't support generic yet.
+type regencyResponse struct {
+	Status  string        `json:"status"`
+	Message string        `json:"message"`
+	Data    model.Regency `json:"data"`
+}
